db6d60dbda3bcad36e85d3fdd67ca447bbdc3316
Daniel Abrahams <dgabrahams@aol.com>
2018-04-17 12:55:01 +0100

    Added Commerce Module, updated DB to use drupal-commerce DB in Mamp

--- Drupal-Files/sites/default/settings.php
+++ Drupal-Files/sites/default/settings.php
@@ -786,7 +786,7 @@ $settings['entity_update_batch_size'] = 50;
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
 $databases['default']['default'] = array (
-  'database' => 'drupal-8-5-1',
+  'database' => 'drupal-commerce',
   'username' => 'root',
   'password' => '',
   'prefix' => '',
